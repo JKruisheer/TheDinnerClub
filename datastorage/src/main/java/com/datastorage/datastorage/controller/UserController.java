f0dfc9a53dce781fdcdf7268e0d92990300125df
Jesse Kruisheer <kruisheer@outlook.com>
2021-07-12 20:56:02 +0200

    #00000|React|Create some form of front end. Basic communication between front and backend at least.

--- datastorage/src/main/java/com/datastorage/datastorage/controller/UserController.java
+++ datastorage/src/main/java/com/datastorage/datastorage/controller/UserController.java
@@ -4,6 +4,7 @@ import com.datastorage.datastorage.entity.User;
 import com.datastorage.datastorage.repository.UserRepository;
 import com.datastorage.datastorage.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("users")
     public List<User> getUsers(){
         return this.userService.getUsers();
